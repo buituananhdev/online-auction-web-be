8304c66810b86d12bbf8f276c706b9d328447650
TuanAnh1003 <anhaanh2003@gmail.com>
2024-03-09 19:36:04 +0700

    docs: update authorize button and add swagger docs for auth and user controller

--- OnlineAuctionWeb.Api/Program.cs
+++ OnlineAuctionWeb.Api/Program.cs
@@ -4,6 +4,7 @@ using OnlineAuctionWeb.Application;
 using OnlineAuctionWeb.Domain;
 using OnlineAuctionWeb.Domain.Mappings;
 using OnlineAuctionWeb.Infrastructure.Middleware;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,26 +23,36 @@ builder.Services.AddSwaggerGen(c =>
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        In = ParameterLocation.Header,
-        Description = "Please enter the token into the field",
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
         Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
     });
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
         {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
             },
-            new string[] { }
-        }
-    });
+            new List<string>()
+          }
+        });
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = System.IO.Path.Combine(System.AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
 });
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
