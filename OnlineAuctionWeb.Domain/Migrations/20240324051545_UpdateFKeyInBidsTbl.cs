c8f749838252bf868a374b61805b2a9139bb32e8
TuanAnh1003 <anhaanh2003@gmail.com>
2024-03-24 12:19:29 +0700

    feat: add bid count in getall auction endpoint

---
+++ OnlineAuctionWeb.Domain/Migrations/20240324051545_UpdateFKeyInBidsTbl.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineAuctionWeb.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFKeyInBidsTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BidderId",
+                table: "Bids");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Bids");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BidderId",
+                table: "Bids",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Bids",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- OnlineAuctionWeb.Domain/Models/Bid.cs
+++ OnlineAuctionWeb.Domain/Models/Bid.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int AuctionId { get; set; }
         public Auction Auction { get; set; }
-        public int BidderId { get; set; }
+        public int UserId { get; set; }
         public User User { get; set; }
         public decimal BidAmount { get; set; }
         public DateTime BidTime { get; set; }
