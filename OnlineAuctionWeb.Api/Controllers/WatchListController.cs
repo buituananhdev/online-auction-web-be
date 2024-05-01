555ad4be582bccbe742fc4adb42e95921a44038f
TuanAnh1003 <anhaanh2003@gmail.com>
2024-05-01 10:42:17 +0700

    feat: add and get watch list api

---
+++ OnlineAuctionWeb.Api/Controllers/WatchListController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using OnlineAuctionWeb.Application.Services;
+using OnlineAuctionWeb.Domain.Dtos;
+using OnlineAuctionWeb.Domain.Enums;
+
+namespace OnlineAuctionWeb.Api.Controllers
+{
+    [Route("api/watchlist")]
+    [ApiController]
+    public class WatchListController : ControllerBase
+    {
+        private readonly IWatchListService _watchListService;
+        public WatchListController(IWatchListService watchListService)
+        {
+            _watchListService = watchListService;
+        }
+
+        /// <summary>
+        /// Add auction to watch list
+        /// </summary>
+        /// <param name="createWatchListDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddToWatchListAsync(CreateWatchListDto createWatchListDto)
+        {
+            await _watchListService.AddToWatchListAsync(createWatchListDto);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get watch list by user ID
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="searchQuery"></param>
+        /// <returns></returns>
+        /// <response code="200">Returns the watch list</response>
+        /// <response code="401">Invalid token</response>
+        /// <response code="500">Internal server error</response>
+        [HttpGet]
+        public async Task<IActionResult> GetWatchListByUserIDAsync(int pageNumber = 1, int pageSize = 10, string? searchQuery = null, WatchListTypeEnum? type = null)
+        {
+            var result = await _watchListService.GetWatchListByUserIDAsync(pageNumber, pageSize, searchQuery, type);
+            return Ok(result);
+        }
+    }
+}
--- OnlineAuctionWeb.Application/Services/WatchListService.cs
+++ OnlineAuctionWeb.Application/Services/WatchListService.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using OnlineAuctionWeb.Application.Exceptions;
 using OnlineAuctionWeb.Domain;
 using OnlineAuctionWeb.Domain.Dtos;
+using OnlineAuctionWeb.Domain.Enums;
 using OnlineAuctionWeb.Domain.Models;
+using OnlineAuctionWeb.Domain.Payloads;
 
 namespace OnlineAuctionWeb.Application.Services
 {
@@ -13,34 +15,43 @@ namespace OnlineAuctionWeb.Application.Services
         Task<List<string>> GetListAuctionIdsByUserIDAsync(int userID);
         Task<List<string>> GetListUserIdsByAuctionIDAsync(int auctionID);
         Task AddToWatchListAsync(CreateWatchListDto createWatchListDto);
+        Task<PaginatedResult<AuctionDto>> GetWatchListByUserIDAsync(
+            int pageNumber,
+            int pageSize,
+            string? searchQuery = null,
+            WatchListTypeEnum? type = null
+       );
     }
     public class WatchListService : IWatchListService
     {
         private readonly DataContext _context;
         private readonly ICurrentUserService _currentUserService;
         private readonly IMapper _mapper;
+        private readonly IFeedbackService _feedbackService;
 
-        public WatchListService(DataContext context, ICurrentUserService currentUserService, IMapper mapper)
+        public WatchListService(DataContext context, ICurrentUserService currentUserService, IMapper mapper, IFeedbackService feedbackService)
         {
             _context = context;
             _currentUserService = currentUserService;
             _mapper = mapper;
+            _feedbackService = feedbackService;
         }
 
         public async Task AddToWatchListAsync(CreateWatchListDto createWatchListDto)
         {
             try
             {
-                if(_currentUserService.UserId == null)
+                if (_currentUserService.UserId == null)
                 {
                     throw new CustomException(StatusCodes.Status401Unauthorized, "Invalid token!");
                 }
 
-                createWatchListDto.UserId = (int)_currentUserService.UserId;
-                bool isExist = _context.WatchList.Any(x => x.UserId == createWatchListDto.UserId && x.AuctionId == createWatchListDto.AuctionId);
-                if(!isExist)
+                var watchlistDto = _mapper.Map<WatchList>(createWatchListDto);
+                watchlistDto.UserId = (int)_currentUserService.UserId;
+                bool isExist = _context.WatchList.Any(x => x.UserId == watchlistDto.UserId && x.AuctionId == watchlistDto.AuctionId);
+                if (!isExist)
                 {
-                    _context.WatchList.Add(_mapper.Map<WatchList>(createWatchListDto));
+                    _context.WatchList.Add(watchlistDto);
                     await _context.SaveChangesAsync();
                 }
             }
@@ -79,7 +90,82 @@ namespace OnlineAuctionWeb.Application.Services
                 .ToListAsync();
 
                 return userIds;
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<PaginatedResult<AuctionDto>> GetWatchListByUserIDAsync(
+            int pageNumber,
+            int pageSize,
+            string? searchQuery = null,
+            WatchListTypeEnum? type = null
+        )
+        {
+            try
+            {
+                var query = _context.WatchList
+                    .Include(w => w.Auction)
+                        .ThenInclude(a => a.Category)
+                    .Include(w => w.Auction)
+                        .ThenInclude(a => a.Bids)
+                    .Include(w => w.Auction)
+                        .ThenInclude(a => a.User)
+                    .Where(w => w.UserId == _currentUserService.UserId);
+
+                if (type != null)
+                {
+                    query = query.Where(w => w.Type == type);
+                }
+
+                if (!string.IsNullOrEmpty(searchQuery))
+                {
+                    query = query.Where(a =>
+                        EF.Functions.Like(a.Auction.ProductName, $"%{searchQuery}%") ||
+                        EF.Functions.Like(a.Auction.Description, $"%{searchQuery}%"));
+                }
+
+                var totalAuctions = await query.CountAsync();
+
+                var auctions = await query
+                    .Select(w => w.Auction)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                var totalPages = (int)Math.Ceiling((double)totalAuctions / pageSize);
+
+                // Map auctions to AuctionDtos
+                var auctionDtos = new List<AuctionDto>();
+                foreach (var auction in auctions)
+                {
+                    var auctionDto = _mapper.Map<AuctionDto>(auction);
+                    auctionDto.BidCount = auction.Bids.Count();
+                    auctionDto.User = _mapper.Map<UserDto>(auction.User);
+                    auctionDto.User.ratings = _feedbackService.GetAverageRatingByUserId(auction.UserId);
+                    auctionDto.Category = _mapper.Map<CategoryDto>(auction.Category);
+                    auctionDtos.Add(auctionDto);
+                }
+
+                var meta = new PaginatedMeta
+                {
+                    CurrentPage = pageNumber,
+                    TotalPages = totalPages,
+                    PageSize = pageSize,
+                };
+
+                var result = new PaginatedResult<AuctionDto>
+                {
+                    Meta = meta,
+                    Data = auctionDtos
+                };
+
+                return result;
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 throw;
