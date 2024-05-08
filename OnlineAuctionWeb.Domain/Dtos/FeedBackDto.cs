85e763cabcffe7661359af3ab7c575cf78f8f0f0
TuanAnh1003 <anhaanh2003@gmail.com>
2024-05-08 22:27:32 +0700

    feat: add base property in feedback dto

--- OnlineAuctionWeb.Domain/Dtos/FeedBackDto.cs
+++ OnlineAuctionWeb.Domain/Dtos/FeedBackDto.cs
@@ -1,4 +1,5 @@
-﻿using OnlineAuctionWeb.Domain.Models;
+﻿using OnlineAuctionWeb.Domain.Common;
+using OnlineAuctionWeb.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace OnlineAuctionWeb.Domain.Dtos
 {
-    public class FeedBackDto
+    public class FeedBackDto : BaseDomainEntity
     {
         public UserDto FromUser { get; set; }
         public int ToUserId { get; set; }
