ab1f4b212be40be55dd3d3d3fb14d8e84984b9e6
TuanAnh1003 <anhaanh2003@gmail.com>
2024-05-10 07:56:16 +0700

    feat: add img url in category

--- OnlineAuctionWeb.Domain/Models/Category.cs
+++ OnlineAuctionWeb.Domain/Models/Category.cs
@@ -7,6 +7,7 @@ namespace OnlineAuctionWeb.Domain.Models
     {
         public string CategoryName { get; set; }
         public StatusEnum Status { get; set; } = StatusEnum.Active;
+        public string? ImageUrl { get; set; }
         public List<Auction> Products { get; set; }
     }
 }
